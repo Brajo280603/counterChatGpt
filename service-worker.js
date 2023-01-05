da69fe1b65f891f20047fd31819483a52444f563
Brajananda Mahata <75053676+Brajo280603@users.noreply.github.com>
2023-01-05 10:54:19 +0530

    Create service-worker.js

---
+++ service-worker.js
@@ -0,0 +1,23 @@
+const CACHE_NAME = 'counter-app-cache';
+const CACHE_URLS = [
+  '/index.html',
+  '/style.css',
+  '/script.js',
+  '/icon.png'
+];
+
+self.addEventListener('install', function(event) {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(CACHE_URLS);
+    })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(response) {
+      return response || fetch(event.request);
+    })
+  );
+});
